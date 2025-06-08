f8dcf8dd9264c252415973754ec432c0a0a82645
SeanKang <seankang@gmail.com>
2025-06-07 17:54:32 -0700

    firebase analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -6,17 +7,35 @@ void main() async { // change to async because we invoke await
   WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized before calling Firebase
   await Firebase.initializeApp();
 
+  // Initialize Firebase
+  logAppOpen(); // This is a custom function to log app open events, you can define it as needed
+
   runApp(const MyApp());
 }
 
+void logAppOpen() {
+  // Here you can implement your logic to log the app open event
+  // For example, you might want to send an event to Firebase Analytics
+  print("App Opened"); // Placeholder for actual logging logic
+  // You can replace this with your logging logic, such as Firebase Analytics
+  FirebaseAnalytics.instance.logAppOpen();
+}
+
 class MyApp extends StatelessWidget {
+
+
   const MyApp({super.key});
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorObservers: <NavigatorObserver>[observer],
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -45,7 +64,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
       _counter++;
     });
+    Map<String, Object>? eventParams = {
+      'counter_value': _counter,
+    };
+
+    // Log the increment event to Firebase Analytics
+    FirebaseAnalytics.instance.logEvent(
+      name: 'increment_counter',
+      parameters: eventParams,
+    );
+    print("Counter incremented to $_counter"); // Optional: Print to console for debugging
+
   }
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    // You can add any initialization logic here if needed
+    // For example, you might want to log a screen view event
+
+    
+    FirebaseAnalytics.instance.setCurrentScreen(screenName: "Home Screen");
+  }
+
 
   @override
   Widget build(BuildContext context) {
